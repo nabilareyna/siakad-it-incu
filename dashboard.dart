23ec9f93345062e02a83b538be817937dd044c80
ojaannnn <147322423+ojaannnn@users.noreply.github.com>
2023-10-18 21:51:01 +0700

    Add files via upload

--- dashboard.dart
+++ dashboard.dart
@@ -7,7 +7,8 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [ SizedBox(height: 10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          SizedBox(height: 10),
           Text(
             "Salam,",
             style: TextStyle(
@@ -110,7 +111,8 @@ class DashboardPage extends StatelessWidget {
                         ),
                       ),
                       Container(
-                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Row(
                               children: [
@@ -203,7 +205,6 @@ class DashboardPage extends StatelessWidget {
           Column(
             children: [
               Container(
-                margin: EdgeInsets.all(24),
                 padding: EdgeInsets.all(20),
                 decoration: BoxDecoration(
                     boxShadow: [
@@ -215,43 +216,187 @@ class DashboardPage extends StatelessWidget {
                     color: Colors.white,
                     border: Border.all(color: Colors.white),
                     borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Column(
+                child: Row(
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Senin",
-                          style: TextStyle(
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        )
-                      ],
+                    Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "Senin",
+                                          style: TextStyle(
+                                              fontFamily: 'Inter',
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "Mata Kuliah",
+                                          style: TextStyle(
+                                              fontFamily: 'Inter',
+                                              fontSize: 9,
+                                              color: Colors.grey),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "Filsafat Ilmu",
+                                          style: TextStyle(
+                                              fontFamily: 'Inter',
+                                              fontSize: 12),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "Pancasila",
+                                          style: TextStyle(
+                                              fontFamily: 'Inter',
+                                              fontSize: 12),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          "Mata Kuliah                                                           Jam                                                          Kelas",
-                          style: TextStyle(fontFamily: 'Inter', fontSize: 9),
-                        )
-                      ],
+                    Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(color: Colors.white),
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      "    ",
+                                      style: TextStyle(
+                                          fontFamily: 'Intel',
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Jam",
+                                      style: TextStyle(
+                                          fontFamily: 'Inter',
+                                          fontSize: 9,
+                                          color: Colors.grey),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "09.00||11.30",
+                                      style: TextStyle(
+                                          fontFamily: 'Inter', fontSize: 11),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "12.20||14.00",
+                                      style: TextStyle(
+                                          fontFamily: 'Inter', fontSize: 11),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          "Filsafat Ilmu                                 09.00|11.00                                 B.317",
-                          style: TextStyle(fontFamily: 'Inter', fontSize: 12),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "Pancasila                                     12.30|14.00                                 B.307",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontFamily: 'Inter', fontSize: 12),
-                        )
-                      ],
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "    ",
+                                style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Gedung",
+                                  style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 9,
+                                      color: Colors.grey))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "B. 307",
+                                style: TextStyle(
+                                    fontFamily: 'Inter', fontSize: 12),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "B. 317",
+                                style: TextStyle(
+                                    fontFamily: 'Inter', fontSize: 12),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
